ea9fba8422d068899f916163efce7c379a52d3fa
Stephan Griesel <sgriesel@gmail.com>
2018-08-26 07:50:52 +0200

    tweenmax background text

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function fadeOut() {
         y:-100,
         opacity:0
     });
-    
+    TweenMax.to(".screen",2, {
+        y: -500,
+        opacity: 0,
+        ease: Power2.easeInOut,
+        delay:1.5
+    });
 }
